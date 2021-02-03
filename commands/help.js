0f969720bfe058b7eae9486e7d9c8f02f6a2ba89
Chaplingund <77300464+Chaplingund@users.noreply.github.com>
2021-02-03 12:50:46 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,31 +1,30 @@
 module.exports = {
   name: "help",
   description: "help",
-   async execute(message, args, Discord, client, config, serverInfo){
+   async execute(message, args, Discord, client, config, fetch, package, serverInfo){
      p = config.prefix;
      const embed = {
-       "title": "Lijst van commando's voor RobotBeerV2!",
-       "description": "Naast deze commando's bevat deze bot nog meer extra features! Wil je weten welke? Contacteer @Chappie#1852 voor meer informatie!",
+       "title": "Lijst van commando's voor " + package.name + "!" ,
        "color": "#ffc603",
        "footer": {
-         "icon_url": "https://i.imgur.com/xTfJeqb.png",
-         "text": "Dit bericht is verzonden met behulp van RobotBeerV2."
+         "icon_url": "https://i.imgur.com/cYtNPbX.png",
+         "text": "Voor verdere vragen contacteer een staff member."
        },
        "thumbnail": {
-         "url": "https://i.imgur.com/7JMbslR.png"
+         "url": "https://i.imgur.com/xhRTM0Q.png"
        },
        "fields": [
          {
-           "name": "Utility",
-           "value": p + "Mail - Verstuurd een anoniem bericht (werkt alleen in dm). \n"+ p +"Tournament - Maakt automatisch tournament channels aan.\n"+ p +"mb-usage - Laat zien hoeveel mb de bot gebruikt."
+           "name": "Informatie",
+           "value": p + "IP - Geeft het IP van de server weer. \n"+ p + "Server - Geeft informatie over de Minecraft server weer. \n"+ p +"Status - Vraagt de status van de APIs en server op.\n" + p + "Whois - Geeft informatie over een Minecraft of Discord account. \n"+ p + "Links - Geeft alle links van ons netwerk weer.\n" + p + "Vacatures - Geeft alle vacatures van ons netwerk weer."
          },
          {
-           "name": "Moderation",
-           "value":  p +"Kick - Kicked een speler. \n"+ p +"Ban - Banned een speler.  \n"+ p +"Mute - Mute een speler. \n"+ p +"Unmute - Unmute een speler. \n"+ p +"Clear - Verwijderd het opgegeven aantal berichten."
+           "name": "Moderatie",
+           "value":  p +"Kick - Kicked een speler van discord. \n"+ p +"Ban - Banned een speler permanent van de Discord.  \n"+ p +"Mute - Mute een speler voor een (on)bepaalde tijd. \n"+ p +"Unmute - Unmute een speler handmatig. \n"+ p +"Clear - Verwijderd het opgegeven aantal berichten."
          },
          {
-           "name": "Other",
-           "value":  p +"Gelebeer - Gebruik" + p + "help gelebeer voor meer informatie. \n"+ p +"Ping - Pong! \n"+ p +"Gans - Kijkt of je wel of geen gans maakt."
+           "name": "Links",
+           "value":  p +"Shop - Geeft de link naar de shop." + p+ "Landen - Geeft de link naar het landen overzicht. \n"+ p + "Discord - Geeft de officieële Discord link weer. \n"+ p + "Dynmap - Geeft de link naar Dynmap van PlanetRisk weer. \n"+ p + "Website - Geeft de link naar de website weer. \n"+ p + "Instagram - Geeft ons Instagram account weer."
          }
        ]
      };
